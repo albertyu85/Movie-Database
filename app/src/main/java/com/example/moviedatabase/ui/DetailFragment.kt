3d8253583fbee00039cda77938ea27838b23b221
Albert Yu <albertyu_85@yahoo.com>
2020-09-16 16:53:53 -0700

    updated detail fragment and added safeargs

--- app/src/main/java/com/example/moviedatabase/ui/DetailFragment.kt
+++ app/src/main/java/com/example/moviedatabase/ui/DetailFragment.kt
@@ -2,10 +2,12 @@ package com.example.moviedatabase.ui
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.moviedatabase.R
 
 class DetailFragment : Fragment() {
@@ -14,13 +16,34 @@ class DetailFragment : Fragment() {
         fun newInstance() = DetailFragment()
     }
 
+    val args : DetailFragmentArgs by navArgs()
     private lateinit var viewModel: DetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        //val args: ConfirmationFragmentArgs by navArgs()
+        //
+        //override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //    val tv: TextView = view.findViewById(R.id.textViewAmount)
+        //    val amount = args.amount
+        //    tv.text = amount.toString()
+        //}
+
+
         return inflater.inflate(R.layout.detail_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val type = args.type
+        val id = args.id
+
+        Log.d("Detail", "Type = $type and id = $id")
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/moviedatabase/ui/adapter/MovieAdapter.kt
+++ app/src/main/java/com/example/moviedatabase/ui/adapter/MovieAdapter.kt
@@ -1,10 +1,17 @@
 package com.example.moviedatabase.ui.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.RecyclerView
+import com.example.moviedatabase.R
 import com.example.moviedatabase.databinding.ItemMovieBinding
 import com.example.moviedatabase.model.NowPlayingMovie
+import com.example.moviedatabase.ui.DetailFragmentArgs
+import com.example.moviedatabase.ui.nowplaying.NowPlayingFragmentDirections
 
 class MovieAdapter : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
@@ -28,6 +35,20 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: MovieAdapter.ViewHolder, position: Int) {
         val item = items[position]
         holder.binding.movie = item
+        //   val action = SpecifyAmountFragmentDirections.confirmationAction(amount)
+        //   v.findNavController().navigate(action)
+        val movie = holder.binding.movie
+        var id = 0
+        if (movie != null)
+            id = movie.id
+        val type = when(holder.binding.movie) {
+            is NowPlayingMovie -> "Now Playing"
+            else -> "Movie"
+        }
+        holder.binding.movieImageView.setOnClickListener {
+            val action = NowPlayingFragmentDirections.actionNowPlayingFragmentToDetailFragment(type, id)
+            it.findNavController().navigate(action)
+        }
     }
 
 
